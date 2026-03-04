b56cf58e365bb4461bdfa218e9552f72a288978f
Deetya Adhikari <adhikarideetya@gmail.com>
2026-03-04 17:32:55 -0500

    cloud platforms and collisions handled

--- game.lua
+++ game.lua
@@ -50,11 +50,13 @@ function _init()
   player.max_jumps = 1
   player.jumps_left = 0
   player.air_grant = false -- prevents re-granting multiple times in same airtime
+  player.has_boots = false
 
   platform_rows = {
     { 3,  4,  5,     6,  7,  8},
     {19, 20, 21,    22, 23, 24},
     {35, 36, 37,    38, 39, 40},
+    {51, 52, 53,    54, 55, 56},
   }
 
   -- rain
@@ -78,11 +80,42 @@ function _init()
   local sw = screensize.width / tilesize  -- 16
   local sh = screensize.height / tilesize -- 16
 
+  local dy = 6
 
   rooms = {
-    [1] = { mapx=0,  mapy=0, wx=0*sw, wy=0, w=20, h=32, spawnx=3, spawny=13 },
-    [2] = { mapx=20, mapy=0, wx=1*sw, wy=0, w=20, h=32, spawnx=2, spawny=13 },
-    [3] = { mapx=0, mapy=0, wx=1*sw, wy=0, w=20, h=32, spawnx=2, spawny=13 },
+    [1] = { mapx=0,  mapy=0-dy, wx=0*sw, wy=0, w=20, h=32, spawnx=3, spawny=13 },
+    [2] = { mapx=20, mapy=0-dy, wx=1*sw, wy=0, w=20, h=32, spawnx=2, spawny=13 },
+    [3] = { mapx=40, mapy=0-dy, wx=1*sw, wy=0, w=20, h=32, spawnx=2, spawny=13 },
+  }
+
+  coins_by_room = {
+    
+    [1] = {
+      -- {x=12, y=22},
+      {x=13, y=10},
+    },
+    [2] = {
+      {x=18, y=10},
+      {x=12, y=18},
+      -- {x=11, y=15},
+    },
+    [3] = {
+      {x=6, y=13},
+    },
+  }
+
+  platforms_by_room = {
+    [1] = {
+      {x=8, y=25, w=3, row=2, variant=2},
+      {x=10, y=21, w=3},
+      {x=5, y=15, w=4},
+      {x=18, y=9, w=4},
+      {x=13, y=11, w=4, row=4, variant=1},
+    },
+    [2] = {
+      {x=1, y=17, w=10},
+      {x=11, y=21, w=3, row=4, variant=1}
+    },
   }
 
   for i = 1, #rooms do
@@ -93,12 +126,7 @@ function _init()
   -- coins
   coin_sprite = 33
   coin_margin = 0.1 -- shrinks coin hitbox
-  coins = {
-    {x=12, y=22},
-    {x=14, y=11},
-    {x=18, y=9},
-    {x=2, y=24},
-  }
+  
   coin_count = 0
 
   -- pickup radius (in tiles).
@@ -116,7 +144,7 @@ function _init()
   -- jumpspeed = 11 / tilesize
   jump_v = 7.5 / tilesize       -- initial upward velocity (try 10-12)
 
-  jump_hold_max = 4      -- extra lift frames while holding jump (try 4-8)
+  jump_hold_max = 3      -- extra lift frames while holding jump (try 4-8)
   jump_cut_mult = 0.45         -- how hard we cut jump on release (0.35-0.7)
 
   player.jump_hold = 0
@@ -137,54 +165,31 @@ function _init()
   load_room(1)
 end
 
-
-
-  
-function draw_player()
-  local rx = room_px()
-  local ry = room_py()
-  local bob = sin(player.bob_t/30) * 0.0002
-  local spr_id = player.anim[player.frame]
-
-  palt(13, true)
-
-  spr(spr_id,
-    rx + player.x * tilesize - 8,
-    ry + player.y * tilesize - 16 + bob,
-    2, 2, player.mirror
-  )
-  
-  pal()
-end
-
-function apply_skin_by_id(s)
-  pal() -- reset first
-
-  if s==1 then
-    -- dj1: BLUE-BLUE
-    pal(bird_bottom_color,12)
-    pal(bird_bottom_outline, 1)
-
-  elseif s==2 then
-    -- dj2: BLUE-RED
-    
-  elseif s==3 then
-    -- sj1: RED-RED
-    pal(bird_top_color,    8)
-    pal(bird_top_outline,  2)
-
-  else
-    -- sj2: RED-BLUE (skin==4)
-    pal(bird_top_color,    8)
-    pal(bird_bottom_color, 12)
-    pal(bird_top_outline,  2)
-    pal(bird_bottom_outline, 1)
-  end
-end
-  
-
 function room_px() return room.wx * tilesize end
 function room_py() return room.wy * tilesize end
+
+function is_cloud_sprite(s)
+  local set = platform_rows[4]
+  if not set then return false end
+  for i=1,#set do
+    if s == set[i] then return true end
+  end
+  return false
+end
+
+
+function start_game()
+  coin_count = 0
+  skin_timer = 0
+  skin = 1
+
+  cam.x = 0
+  cam.y = 0
+
+  load_room(1)
+end
+
+
 
 function set_room(id)
   room_id = id
@@ -232,10 +237,11 @@ end
 
 function update_coins()
   -- use the player's "horizontal" body box (good general hitbox)
+  local list = coins_by_room and coins_by_room[room_id] or {}
   local b = player.collision.box.horizontal
 
-  for i=#coins,1,-1 do
-    local c = coins[i]
+  for i =# list, 1, -1 do
+    local c = list[i]
 
     -- coin is a 1x1 tile box in tile coords
     local cl = c.x - coin_margin
@@ -246,12 +252,21 @@ function update_coins()
     -- overlap test
     if b.right > cl and b.left < cr and b.bottom > ct and b.top < cb then
       sfx(8)
-      deli(coins, i)
+      deli(list, i) --delete from list of this room's coins
       coin_count += 1
       
     end
     
   end
+end
+
+function draw_coins(rx, ry) 
+  local list = coins_by_room and coins_by_room[room_id] or {}
+  for c in all(list) do
+    palt(0,true)
+    spr(coin_sprite, rx + c.x*tilesize, ry + c.y*tilesize)
+  end
+  pal()
 end
 
 function update_rain()
@@ -312,7 +327,7 @@ function draw_one_platform(x, y, w, row, variant)
 
   if w == 1 then
     -- single-tile platform: just use middle
-    spr(m, rx + x*8, ry + y*8)
+    spr(mid, rx + x*8, ry + y*8)
     return
   end
 
@@ -358,25 +373,73 @@ end
 function set_platforms_for_room()
   local list = platforms_by_room[room_id] or {}
   for p in all(list) do
-    set_platform(p.x, p.y, p.w, p.row, p.var)
+    p.flash = 0
+    p._lastv = nil
+    set_platform(p.x, p.y, p.w, p.row, p.variant)
   end
 end
 
-platforms_by_room = {
-  [1] = {
-    {x=2, y=20, w=3},
-    {x=5, y=14, w=6},
-    {x=1, y=10, w=14},
-  },
-  [2] = {
-    {x=1, y=18, w=10},
-  },
-}
+function player_touches_platform(p)
+  local b = player.collision.box.horizontal
+
+  local pl = p.x
+  local pr = p.x + p.w
+  local pt = p.y
+  local pb = p.y + 1
+
+  return b.right > pl and b.left < pr and b.bottom > pt and b.top < pb
+end
+
+function player_on_platform(p)
+  -- p.x/p.y/p.w are in tile coords relative to room
+  return player.onground
+    and abs(player.y - p.y) < 0.02
+    and player.x >= p.x
+    and player.x < p.x + p.w
+end
+
+
 
 function draw_platforms()
   local list = platforms_by_room[room_id] or {}
   for p in all(list) do
-    draw_one_platform(p.x, p.y, p.w, p.row, p.var)
+    local v = p.variant or 1
+    if (p.row or 1) == 4 then
+       v = (p.flash and p.flash > 0) and 2 or 1
+    end
+    draw_one_platform(p.x, p.y, p.w, p.row, v)
+  end
+end
+
+function repaint_clouds()
+  local list = platforms_by_room[room_id] or {}
+
+  for p in all(list) do
+    if (p.row or 1) == 4 then
+      -- countdown
+      if p.flash and p.flash > 0 then
+        p.flash -= 1
+      else
+        p.flash = 0
+      end
+
+      local touched = player_touches_platform(p)
+      local stood   = player_on_platform(p)
+
+      -- boots=true: NOT solid, but touching should flash
+      -- boots=false: (your collisions already handle solidity) touching/standing should flash too
+      if touched or stood then
+        p.flash = 30 -- 0.5 seconds
+      end
+
+      local v = (p.flash > 0) and 2 or 1
+
+      -- write to map so it visually changes even under map()
+      if p._lastv ~= v then
+        set_platform(p.x, p.y, p.w, p.row, v)
+        p._lastv = v
+      end
+    end
   end
 end
 
@@ -439,6 +502,7 @@ function game_update()
   end
 
   applyphysics(player)
+  repaint_clouds()
   update_coins()
   animate(player)
 
@@ -566,6 +630,7 @@ function applyphysics(entity)
 
   local wasonground = entity.onground
   entity.onground = false
+  entity.just_landed = false
 
   
   -- increase precision by applying physics in smaller steps
@@ -577,85 +642,77 @@ function applyphysics(entity)
     steps = ceil(highestspeed / 0.25)
   end
   
-  for i = 1, steps do
+  for i=1,steps do
+    local prevy = entity.y
+
     entity.x += speed.x / steps
     entity.y += speed.y / steps
-    
+
     updatecollisionbox(entity)
-    
-    -- slope collisions
+
+    -- slope collisions (only slopes)
     for tile in gettiles(entity, "floor") do
       if tile.slope then
         local tiletop = tile.y
-      
-        if tile.slope then
-          local slope = tile.slope
-          local xoffset = entity.x - tile.x
-          
-          if xoffset < 0 or xoffset > 1 then
-            -- only do slopes if the entity's center x coordinate is inside the tile space
-            -- otherwise ignore this tile
-            tiletop = nil
-          else
-            local alpha
-            if slope.reversed then
-              alpha = 1 - xoffset
-            else
-              alpha = xoffset
-            end
-            
-            local slopeheight = lerp(slope.offset, slope.offset + slope.height, alpha)
-            tiletop = tile.y + 1 - slopeheight
-            
-            -- only snap the entity down to the slope's height if it wasn't jumping or on the ground
-            if entity.y < tiletop and speed.y < 0 then
-              tiletop = nil
-            end
-          end
-        else
+        local slope = tile.slope
+        local xoffset = entity.x - tile.x
+
+        if xoffset < 0 or xoffset > 1 then
           tiletop = nil
+        else
+          local alpha = slope.reversed and (1 - xoffset) or xoffset
+          local slopeheight = lerp(slope.offset, slope.offset + slope.height, alpha)
+          tiletop = tile.y + 1 - slopeheight
+
+          -- don't snap upward through slope while jumping
+          if entity.y < tiletop and speed.y < 0 then
+            tiletop = nil
+          end
         end
-        
+
         if tiletop then
           speed.y = 0
           entity.y = tiletop
           entity.onground = true
+          if not wasonground then entity.just_landed = true end
           entity.air_grant = false
           entity.jumps_left = entity.max_jumps - 1
           entity.jump_hold = 0
           entity.jumping = false
           fallingframes = 0
-          entity.jumps_left = entity.max_jumps - 1
-
         end
       end
     end
-    
-    updatecollisionbox(entity)
-    
-    -- wall collisions
-    for tile in gettiles(entity, "horizontal") do
-      if tile.solid and not tile.slope then
-        entity.jumps_left = entity.max_jumps - 1
 
+    updatecollisionbox(entity)
+
+    -- wall collisions (ignore clouds)
+    for tile in gettiles(entity, "horizontal") do
+      if tile.solid and not tile.slope and not tile.cloud then
+        entity.jumps_left = entity.max_jumps - 1
         if entity.x < tile.x + 0.5 then
-          -- push out to the left
           entity.x = tile.x - entity.collision.size.horizontal.width / 2
         else
-          -- push out to the right
           entity.x = tile.x + 1 + entity.collision.size.horizontal.width / 2
         end
       end
     end
-    
+
     updatecollisionbox(entity)
-    
-    -- floor collisions
+
+    -- floor collisions (clouds are one-way + require boots)
     for tile in gettiles(entity, "floor") do
-      if tile.solid and not tile.slope then
+      local solid_here = tile.solid and not tile.slope
+
+      if tile.cloud then
+        solid_here = (entity.has_boots) and speed.y >= 0 and prevy <= tile.y
+      end
+
+      if solid_here then
         speed.y = 0
         entity.y = tile.y
         entity.onground = true
+        if not wasonground then entity.just_landed = true end
         entity.air_grant = false
         entity.jumps_left = entity.max_jumps - 1
         entity.jumping = false
@@ -663,12 +720,12 @@ function applyphysics(entity)
         fallingframes = 0
       end
     end
-    
+
     updatecollisionbox(entity)
-    
-    -- ceiling collisions
+
+    -- ceiling collisions (ignore clouds)
     for tile in gettiles(entity, "ceiling") do
-      if tile.solid and not tile.slope then
+      if tile.solid and not tile.slope and not tile.cloud then
         speed.y = 0
         entity.y = tile.y + 1 + entity.collision.size.vertical.height
         entity.jumping = false
@@ -676,8 +733,7 @@ function applyphysics(entity)
     end
   end
 end
-
--- gets all tiles that might be intersecting entity's collision box
+  -- gets all tiles that might be intersecting entity's collision box
 function gettiles(entity, boxtype)
   local box = entity.collision.box[boxtype]
   local left, top, right, bottom =
@@ -699,6 +755,7 @@ function gettiles(entity, boxtype)
     if sprite > 0 then
       local flags = fget(sprite)
       ret.solid = band(flags, 1) == 1 
+      ret.cloud = is_cloud_sprite(sprite)
 
       if band(flags, 128) == 128 then
         -- this is a slope if flag 7 is set
@@ -711,6 +768,7 @@ function gettiles(entity, boxtype)
 
     else
       ret.solid = false
+      ret.cloud = false
     end
     x += 1
     if x > right then
@@ -747,6 +805,49 @@ function updatecollisionbox(entity)
   }
 end
 
+  
+function draw_player()
+  local rx = room_px()
+  local ry = room_py()
+  local bob = sin(player.bob_t/30) * 0.0002
+  local spr_id = player.anim[player.frame]
+
+  palt(13, true)
+
+  spr(spr_id,
+    rx + player.x * tilesize - 8,
+    ry + player.y * tilesize - 16 + bob,
+    2, 2, player.mirror
+  )
+  
+  pal()
+end
+
+function apply_skin_by_id(s)
+  pal() -- reset first
+
+  if s==1 then
+    -- dj1: BLUE-BLUE
+    pal(bird_bottom_color,12)
+    pal(bird_bottom_outline, 1)
+
+  elseif s==2 then
+    -- dj2: BLUE-RED
+    
+  elseif s==3 then
+    -- sj1: RED-RED
+    pal(bird_top_color,    8)
+    pal(bird_top_outline,  2)
+
+  else
+    -- sj2: RED-BLUE (skin==4)
+    pal(bird_top_color,    8)
+    pal(bird_bottom_color, 12)
+    pal(bird_top_outline,  2)
+    pal(bird_bottom_outline, 1)
+  end
+end
+
 function game_draw()
   
   camera(cam.x, cam.y)
@@ -764,11 +865,7 @@ function game_draw()
   local rx = room_px()
   local ry = room_py()
 
-  for c in all(coins) do
-    palt(0,true)    
-    spr(coin_sprite, rx + c.x*tilesize, ry + c.y*tilesize)
-  end
-  pal()
+  draw_coins(rx, ry)
 
   draw_platforms()
 
@@ -797,7 +894,7 @@ end
 
 -------------------------------------------------------------------------
 
-scene="game" -- or "title"
+scene="title" -- or "title"
 
 function _update()
   if scene=="title" then title_update()
@@ -810,10 +907,14 @@ function _draw()
 end
 
 function title_update()
-  if btnp(4) then scene="game" end
+  if btnp(4) or btnp(5) then
+    start_game()
+    scene="game"
+  end
 end
 
 function title_draw()
   cls(1)
-  print("press ❎", 50, 60, 7)
+  print("birby", 34, 40, 7)
+  print("press ❎ to start", 32, 70, 6)
 end
